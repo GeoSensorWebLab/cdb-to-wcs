6e886b2250679487eb8c41c33faa9b3f9b813722
James Badger <james@jamesbadger.ca>
2017-07-20 17:30:54 -0600

    Extract lossy check to module

--- lib/cdb.rb
+++ lib/cdb.rb
@@ -6,4 +6,9 @@ require "cdb/uref"
 require "cdb/version"
 
 module CDB
+  # Returns the filetype expected for a dataset ID.
+  # Basically Imagery is JPEG2000 and everything else is GeoTIFF.
+  def self.filetype_for_dataset_id(dataset_id)
+    dataset_id.include?("004_Imagery") ? "jp2" : "tif"
+  end
 end
